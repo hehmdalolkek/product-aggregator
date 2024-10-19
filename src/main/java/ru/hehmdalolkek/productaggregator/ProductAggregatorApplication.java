016b9ae09aff4c8b3238c95e84d46a77a5c27613
hehmdalolkek <badeha.inna@yandex.ru>
2024-10-19 10:09:53 +0400

    initial commit

---
+++ src/main/java/ru/hehmdalolkek/productaggregator/ProductAggregatorApplication.java
@@ -0,0 +1,13 @@
+package ru.hehmdalolkek.productaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductAggregatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hehmdalolkek/productaggregator/ProductAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hehmdalolkek.productaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
